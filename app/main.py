bdd20ba9c5ec4154cd789d32093985d4be4707e2
Caua Veiga <cauaveiga@Cauas-MacBook-Air.local>
2022-04-23 08:29:48 +0100

    test

--- app/main.py
+++ app/main.py
@@ -67,6 +67,6 @@ def pallete():
         return 'Error processing image', 500
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 9000))
-    app.run(host='0.0.0.0', port=port)
+#if __name__ == "__main__":
+#    port = int(os.environ.get('PORT', 9000))
+#    app.run(host='0.0.0.0', port=port)
